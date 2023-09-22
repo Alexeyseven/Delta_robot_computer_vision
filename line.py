33fb3d4be71dcb655f6bc75e0c6b4e97cd323440
Alexeyseven <alexeyseven77@gmail.com>
2023-09-22 11:53:27 +0300

    rrbbrb

--- line.py
+++ line.py
@@ -1,22 +1,27 @@
 import cv2
 import numpy as np
 import socket
+import os
 
 
-cap = cv2.VideoCapture('http://192.168.43.1:4747/video')
+#cap = cv2.VideoCapture('http://192.168.43.1:4747/video')
+cap = cv2.VideoCapture(0)
 
 s = socket.socket()
-s.bind(('192.168.1.241', 9090))
+s.bind(('127.0.0.1', 9090))
 s.listen(1)
+os.system('start cmd /k python robot.py')
 conn, addr = s.accept()
 
 
 while True:
+    mes = conn.recv(1024)
+    conn.send(b'ok')
+
     
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
+    
 cap.release()
 cv2.destroyAllWindows()
--- robot.py
+++ robot.py
@@ -2,5 +2,14 @@ import socket
 
 
 s = socket.socket()
-s.connect(('192.168.1.241', 9090))
+s.connect(('127.0.0.1', 9090))
 
+i = 0
+
+while True:
+    s.send(str.encode(str(i)))
+    mes = s.recv(1024)
+    i += 1
+
+    if i == 1000:
+        i = 0
