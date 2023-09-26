fd4b61e489e269ca74884a9278df3d14c2a44a89
Alexeyseven <alexeyseven77@gmail.com>
2023-09-26 10:35:21 +0300

    v1

--- line.py
+++ line.py
@@ -1,37 +1,46 @@
 import cv2
 import numpy as np
 import socket
+import os
+import time
 
 
 def pass_func(x):
    pass
 
-cv2.namedWindow('frame')
 cv2.namedWindow('mask')
+cv2.namedWindow('frame')
 
-x_detect = 100
-stack = []
-robot_move = False
-j = 0
 
-#cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('http://192.168.43.1:4747/video')
+cap = cv2.VideoCapture('Video.mp4')
+#cap = cv2.VideoCapture('http://192.168.43.1:4747/video')
 
-cv2.createTrackbar('HL', 'mask', 11, 255, pass_func)
-cv2.createTrackbar('SL', 'mask', 73, 255, pass_func)
-cv2.createTrackbar('VL', 'mask', 173, 255, pass_func)
-cv2.createTrackbar('HM', 'mask',23, 255, pass_func)
+cv2.createTrackbar('HL', 'mask', 0, 255, pass_func)
+cv2.createTrackbar('SL', 'mask', 50, 255, pass_func)
+cv2.createTrackbar('VL', 'mask', 150, 255, pass_func)
+cv2.createTrackbar('HM', 'mask',70, 255, pass_func)
 cv2.createTrackbar('SM', 'mask', 255, 255, pass_func)
 cv2.createTrackbar('VM', 'mask', 255, 255, pass_func)
 
+cv2.createTrackbar('dp', 'frame', 350, 1000, pass_func)
+cv2.createTrackbar('minDist', 'frame', 16, 100, pass_func)
+cv2.createTrackbar('param1', 'frame', 10, 500, pass_func)
+cv2.createTrackbar('param2', 'frame', 2, 100, pass_func)
+cv2.createTrackbar('minRadius', 'frame', 18, 100, pass_func)
+cv2.createTrackbar('maxRadius', 'frame', 28, 200, pass_func)
+
 s = socket.socket()
-s.bind(('192.168.1.241', 9090))
+s.bind(('127.0.0.1', 9090))
 s.listen(1)
+os.system('start cmd /k python robot.py')
 conn, addr = s.accept()
+
+j = 0
 
 
 while True:
     mes = conn.recv(1024)
+    i = int(mes.decode('utf-8'))
 
     hl = cv2.getTrackbarPos('HL','mask')
     sl = cv2.getTrackbarPos('SL','mask')
@@ -40,54 +49,49 @@ while True:
     sm = cv2.getTrackbarPos('SM','mask')
     vm = cv2.getTrackbarPos('VM','mask')
 
+    c_dp = cv2.getTrackbarPos('dp','frame')
+    c_minDist = cv2.getTrackbarPos('minDist','frame')
+    c_param1 = cv2.getTrackbarPos('param1','frame')
+    c_param2 = cv2.getTrackbarPos('param2','frame')
+    c_minRadius = cv2.getTrackbarPos('minRadius','frame')
+    c_maxRadius = cv2.getTrackbarPos('maxRadius','frame')
+
     hsv_min = np.array((hl, sl, vl), np.uint8)
     hsv_max = np.array((hm, sm, vm), np.uint8)
-   
-    ret, frame = cap.read()
-    flag, track = cap.read()
 
-    cropped = frame[0:480, 0:480]
-    hsv = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
+    ret, frame = cap.read()
+
+    final_wide = 500
+    r = float(final_wide) / frame.shape[1]
+    dim = (final_wide, int(frame.shape[0] * r))
+    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+    frame = frame[200:590, 0:500]
+    mask = frame.copy()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, hsv_min, hsv_max)
 
-    cv2.line(frame, (x_detect, 0), (x_detect, frame.shape[1]), (255, 0, 0), 3)
-    
-    if mes == b'robot_move':
-        print(mes)
-        robot_move = True
-    else:
-        i = int(mes.decode('utf-8'))    
+    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT_ALT, dp = (c_dp/100), minDist = c_minDist, param1 = c_param1, 
+                            param2 = (c_param2/100), minRadius = c_minRadius, maxRadius = c_maxRadius)
+
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+
+        for (x, y, r) in circles:
+            #cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+            if y + 24 < frame.shape[0] and y - 24 > 0 and x + 24 < frame.shape[1] and x -24 > 0:
+                #cv2.imshow('obj', frame[y-24:y+24, x-24:x+24])
+                cv2.imwrite(f'{j}.jpg', frame[y-24:y+24, x-24:x+24])
+                j += 1
             
-    if len(stack)>0 and robot_move==True:
-        translation = stack.pop(0)
-        translation = str(translation[0]) + ', ' + str(translation[1])
-        conn.send(str.encode(translation))
-        robot_move = False
-
-
-    contours, _ = cv2.findContours(mask, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        if cv2.contourArea(cnt)>5000:
-            [x,y,w,h] = cv2.boundingRect(cnt)
-            if x == x_detect:
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
-                cv2.imshow('object', frame[y:y+128, x:x+128])
-                delay = i-j
-                if delay > 50:
-                    delay = 50
-                j = i
-                stack.append([y, delay])
-            else:
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
+            
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
+    time.sleep(0.1)
     
     conn.send(b'none')
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
+    
 cap.release()
 cv2.destroyAllWindows()
