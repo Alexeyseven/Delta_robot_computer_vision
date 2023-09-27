d8577989d362df103864a927903ae37799c8dd1b
Alexeyseven <alexeyseven77@gmail.com>
2023-09-27 12:24:23 +0300

    runtime

--- line.py
+++ line.py
@@ -3,6 +3,7 @@ import numpy as np
 import socket
 import os
 import time
+import tensorflow as tf
 import random
 
 
@@ -12,6 +13,7 @@ def pass_func(x):
 cv2.namedWindow('mask')
 cv2.namedWindow('frame')
 
+model = tf.keras.saving.load_model("myaphly_model_local.keras")
 
 cap = cv2.VideoCapture('Video.mp4')
 #cap = cv2.VideoCapture('http://192.168.43.1:4747/video')
@@ -28,7 +30,7 @@ cv2.createTrackbar('minDist', 'frame', 16, 100, pass_func)
 cv2.createTrackbar('param1', 'frame', 10, 500, pass_func)
 cv2.createTrackbar('param2', 'frame', 2, 100, pass_func)
 cv2.createTrackbar('minRadius', 'frame', 18, 100, pass_func)
-cv2.createTrackbar('maxRadius', 'frame', 24, 200, pass_func)
+cv2.createTrackbar('maxRadius', 'frame', 28, 200, pass_func)
 
 s = socket.socket()
 s.bind(('127.0.0.1', 9090))
@@ -37,67 +39,104 @@ os.system('start cmd /k python robot.py')
 conn, addr = s.accept()
 
 j = 0
+snap_x = 390
+stack = []
+robot_move = False
+first_iter = True
 
 
 while True:
-    mes = conn.recv(1024)
-    i = int(mes.decode('utf-8'))
+   mes = conn.recv(1024)
 
-    hl = cv2.getTrackbarPos('HL','mask')
-    sl = cv2.getTrackbarPos('SL','mask')
-    vl = cv2.getTrackbarPos('VL','mask')
-    hm = cv2.getTrackbarPos('HM','mask')
-    sm = cv2.getTrackbarPos('SM','mask')
-    vm = cv2.getTrackbarPos('VM','mask')
+   if mes == b'robot move':
+      robot_move = True
+   else:
+      i = int(mes.decode('utf-8'))   
 
-    c_dp = cv2.getTrackbarPos('dp','frame')
-    c_minDist = cv2.getTrackbarPos('minDist','frame')
-    c_param1 = cv2.getTrackbarPos('param1','frame')
-    c_param2 = cv2.getTrackbarPos('param2','frame')
-    c_minRadius = cv2.getTrackbarPos('minRadius','frame')
-    c_maxRadius = cv2.getTrackbarPos('maxRadius','frame')
+   if robot_move and len(stack) > 0:
+      print('robot move')
+      send_mes = stack.pop(0)
+      send_mes = str.encode(str(send_mes[0]) + ',' + str(send_mes[1]))
+      robot_move = False
+   else:
+      send_mes = b'none'   
 
-    hsv_min = np.array((hl, sl, vl), np.uint8)
-    hsv_max = np.array((hm, sm, vm), np.uint8)
+   hl = cv2.getTrackbarPos('HL','mask')
+   sl = cv2.getTrackbarPos('SL','mask')
+   vl = cv2.getTrackbarPos('VL','mask')
+   hm = cv2.getTrackbarPos('HM','mask')
+   sm = cv2.getTrackbarPos('SM','mask')
+   vm = cv2.getTrackbarPos('VM','mask')
 
-    ret, frame = cap.read()
+   c_dp = cv2.getTrackbarPos('dp','frame')
+   c_minDist = cv2.getTrackbarPos('minDist','frame')
+   c_param1 = cv2.getTrackbarPos('param1','frame')
+   c_param2 = cv2.getTrackbarPos('param2','frame')
+   c_minRadius = cv2.getTrackbarPos('minRadius','frame')
+   c_maxRadius = cv2.getTrackbarPos('maxRadius','frame')
 
-    final_wide = 500
-    r = float(final_wide) / frame.shape[1]
-    dim = (final_wide, int(frame.shape[0] * r))
-    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-    frame = frame[200:590, 0:500]
-    mask = frame.copy()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, hsv_min, hsv_max)
+   hsv_min = np.array((hl, sl, vl), np.uint8)
+   hsv_max = np.array((hm, sm, vm), np.uint8)
 
-    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT_ALT, dp = (c_dp/100), minDist = c_minDist, param1 = c_param1, 
-                            param2 = (c_param2/100), minRadius = c_minRadius, maxRadius = c_maxRadius)
+   ret, frame = cap.read()
 
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
+   final_wide = 500
+   r = float(final_wide) / frame.shape[1]
+   dim = (final_wide, int(frame.shape[0] * r))
+   frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+   frame = frame[200:590, 0:500]
+   mask = frame.copy()
+   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+   mask = cv2.inRange(hsv, hsv_min, hsv_max)
 
-        for (x, y, r) in circles:
-            r1 = random.randint(-12, 12)
-            r2 = random.randint(0, 1)
-            if r2 == 0:
-               cv2.circle(frame, (x, y), r, (0, 0, 0), 1)
+   #cv2.line(frame, (snap_x, 0), (snap_x, frame.shape[1]), (255, 0, 0), 3)
+
+   circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT_ALT, 
+                              dp = (c_dp/100), 
+                              minDist = c_minDist, 
+                              param1 = c_param1, 
+                              param2 = (c_param2/100), 
+                              minRadius = c_minRadius, 
+                              maxRadius = c_maxRadius)
+
+   if circles is not None:
+      circles = np.round(circles[0, :]).astype("int")
+      r1 = random.randint(0, 48)
+      r2 = random.randint(-12, 12)
+      for (x, y, r) in circles:
+         if abs(snap_x - x) <= 10:
             if y + 24 < frame.shape[0] and y - 24 > 0 and x + 24 < frame.shape[1] and x -24 > 0:
-                if r2 == 1:
-                  cv2.circle(frame, (x-r1, y-r1), r-18, (0, 0, 0), 1)
-                #cv2.imshow('obj', frame[y-24:y+24, x-24:x+24])
-                cv2.imwrite(f'train_images/bad/{j}.jpg', frame[y-24:y+24, x-24:x+24])
-                j += 1
-            
-            
-    cv2.imshow('frame', frame)
-    cv2.imshow('mask', mask)
-    time.sleep(0.1)
-    
-    conn.send(b'none')
+               img = frame[y-24:y+24, x-24:x+24]
+               if r1 == 4:
+                  cv2.circle(frame, (x-r2, y-r2), 4, (0, 0, 0), 1)
+               img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+               img = np.expand_dims(img, axis = 0)
+               predict = model.predict(img)
+               if predict[0][0] > 0.5:
+                  cv2.circle(frame, (x, y), r, (0, 0, 255), 4)
+                  if i - j > 100 or first_iter:
+                     delay = 100
+                     first_iter = False
+                  else:
+                     delay = i - j
+                  stack.append([delay, y])
+                  print(stack)
+                  j = i
+               else:
+                  cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+   cv2.line(frame, (snap_x, 0), (snap_x, frame.shape[1]), (255, 0, 0), 3)
+
+   cv2.imshow('frame', frame)
+   cv2.imshow('mask', mask)
+
+   time.sleep(0.05)
+    
+   conn.send(send_mes)
+
+   if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
     
 cap.release()
 cv2.destroyAllWindows()
--- robot.py
+++ robot.py
@@ -6,12 +6,30 @@ s.connect(('127.0.0.1', 9090))
 print('Robot ready')
 
 i = 0
+j = 0
+delay = 5
+y = 0
+y_prev = 0
 
 while True:
-    s.send(str.encode(str(i)))
+    if i == j + delay:
+        s.send(b'robot move')
+        if y != y_prev or delay > 0:
+            print('robot move, y = ', y, 'delay = ', delay)
+            y_prev = y
+    else:
+        s.send(str.encode(str(i)))
+        
     i += 1
 
     if i == 1000:
         i = 0
 
     mes = s.recv(1024)
+
+    if mes != b'none':
+        line = mes.decode('utf-8').split(',')
+        delay = int(line[0])
+        y = int(line[1])
+        j = i
+        
