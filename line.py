65b7e0bd6856899f05c0b074b0fd207f82863b54
Alexeyseven <alexeyseven77@gmail.com>
2023-10-13 16:29:51 +0300

    synchronus mode

--- line.py
+++ line.py
@@ -2,10 +2,8 @@ import cv2
 import numpy as np
 import socket
 import os
-import time
 import tensorflow as tf
 import random
-from tkinter import *
 
 
 def pass_func(x):
@@ -18,6 +16,10 @@ def transport_start():
 
 def transport_stop():
    pass
+
+
+def diapason_transpole(max_vis, min_vis, max_rob, min_rob, coordinate):
+   return((max_rob-min_rob)*(coordinate-min_vis)/(max_vis-min_vis)+min_rob)
 
 
 cv2.namedWindow('mask')
@@ -43,112 +45,92 @@ cv2.createTrackbar('minRadius', 'frame', 18, 100, pass_func)
 cv2.createTrackbar('maxRadius', 'frame', 28, 200, pass_func)
 
 s = socket.socket()
-s.bind(('127.0.0.1', 9090))
+s.bind(('192.168.1.241', 502))
 s.listen(1)
 os.system('start cmd /k python robot.py')
 conn, addr = s.accept()
+conn.settimeout(0.05)
 
-j = 0
-snap_x = 390
-stack = []
-robot_move = False
-first_iter = True
+snap = False
+min_vis = 0
+max_vis = 400
+min_rob = -480
+max_rob = -130
 
-root = Tk()
-root.geometry('1000x40+40+650')
-Button(text='Транспорт старт', command=transport_start, height=2).place(x=0, y=0)
-Button(text='Транспорт стоп', command=transport_stop, height=2).place(x=100, y=0)
 
 while True:
-   mes = conn.recv(1024)
+   try:
+      mes = conn.recv(1024)
+      if mes == b'snap\r\n':
+         snap = True
+         print(snap)
+      else:
+         print('robot say', mes)   
+   except socket.timeout: 
 
-   if mes == b'robot move':
-      robot_move = True
-   else:
-      i = int(mes.decode('utf-8'))   
+      hl = cv2.getTrackbarPos('HL','mask')
+      sl = cv2.getTrackbarPos('SL','mask')
+      vl = cv2.getTrackbarPos('VL','mask')
+      hm = cv2.getTrackbarPos('HM','mask')
+      sm = cv2.getTrackbarPos('SM','mask')
+      vm = cv2.getTrackbarPos('VM','mask')
 
-   if robot_move and len(stack) > 0:
-      print('robot move')
-      send_mes = stack.pop(0)
-      send_mes = str.encode(str(send_mes[0]) + ',' + str(send_mes[1]))
-      robot_move = False
-   else:
-      send_mes = b'none'   
+      c_dp = cv2.getTrackbarPos('dp','frame')
+      c_minDist = cv2.getTrackbarPos('minDist','frame')
+      c_param1 = cv2.getTrackbarPos('param1','frame')
+      c_param2 = cv2.getTrackbarPos('param2','frame')
+      c_minRadius = cv2.getTrackbarPos('minRadius','frame')
+      c_maxRadius = cv2.getTrackbarPos('maxRadius','frame')
 
-   hl = cv2.getTrackbarPos('HL','mask')
-   sl = cv2.getTrackbarPos('SL','mask')
-   vl = cv2.getTrackbarPos('VL','mask')
-   hm = cv2.getTrackbarPos('HM','mask')
-   sm = cv2.getTrackbarPos('SM','mask')
-   vm = cv2.getTrackbarPos('VM','mask')
+      hsv_min = np.array((hl, sl, vl), np.uint8)
+      hsv_max = np.array((hm, sm, vm), np.uint8)
 
-   c_dp = cv2.getTrackbarPos('dp','frame')
-   c_minDist = cv2.getTrackbarPos('minDist','frame')
-   c_param1 = cv2.getTrackbarPos('param1','frame')
-   c_param2 = cv2.getTrackbarPos('param2','frame')
-   c_minRadius = cv2.getTrackbarPos('minRadius','frame')
-   c_maxRadius = cv2.getTrackbarPos('maxRadius','frame')
+      ret, frame = cap.read()
 
-   hsv_min = np.array((hl, sl, vl), np.uint8)
-   hsv_max = np.array((hm, sm, vm), np.uint8)
+      final_wide = 500
+      r = float(final_wide) / frame.shape[1]
+      dim = (final_wide, int(frame.shape[0] * r))
+      frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+      frame = frame[200:590, 0:500]
+      mask = frame.copy()
+      hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+      mask = cv2.inRange(hsv, hsv_min, hsv_max)
 
-   ret, frame = cap.read()
+      #cv2.line(frame, (snap_x, 0), (snap_x, frame.shape[1]), (255, 0, 0), 3)
 
-   final_wide = 500
-   r = float(final_wide) / frame.shape[1]
-   dim = (final_wide, int(frame.shape[0] * r))
-   frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-   frame = frame[200:590, 0:500]
-   mask = frame.copy()
-   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-   mask = cv2.inRange(hsv, hsv_min, hsv_max)
+      circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT_ALT, 
+                                 dp = (c_dp/100), 
+                                 minDist = c_minDist, 
+                                 param1 = c_param1, 
+                                 param2 = (c_param2/100), 
+                                 minRadius = c_minRadius, 
+                                 maxRadius = c_maxRadius)
 
-   #cv2.line(frame, (snap_x, 0), (snap_x, frame.shape[1]), (255, 0, 0), 3)
-
-   circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT_ALT, 
-                              dp = (c_dp/100), 
-                              minDist = c_minDist, 
-                              param1 = c_param1, 
-                              param2 = (c_param2/100), 
-                              minRadius = c_minRadius, 
-                              maxRadius = c_maxRadius)
-
-   if circles is not None:
-      circles = np.round(circles[0, :]).astype("int")
-      r1 = random.randint(0, 48)
-      r2 = random.randint(-12, 12)
-      for (x, y, r) in circles:
-         if abs(snap_x - x) <= 10:
-            if y + 24 < frame.shape[0] and y - 24 > 0 and x + 24 < frame.shape[1] and x -24 > 0:
-               img = frame[y-24:y+24, x-24:x+24]
-               if r1 == 4:
-                  cv2.circle(frame, (x-r2, y-r2), 4, (0, 0, 0), 1)
-               img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-               img = np.expand_dims(img, axis = 0)
-               predict = model.predict(img)
-               if predict[0][0] > 0.5:
-                  cv2.circle(frame, (x, y), r, (0, 0, 255), 4)
-                  if i - j > 100 or first_iter:
-                     delay = 100
-                     first_iter = False
+      if circles is not None:
+         circles = np.round(circles[0, :]).astype("int")
+         r2 = random.randint(-12, 12)
+         if snap:
+            for (x, y, r) in circles:
+               if y + 24 < frame.shape[0] and y - 24 > 0 and x + 24 < frame.shape[1] and x -24 > 0:
+                  img = frame[y-24:y+24, x-24:x+24]
+                  r1 = random.randint(0, 50)
+                  if r1 == 4:
+                     cv2.circle(frame, (x-r2, y-r2), 4, (0, 0, 0), 1)
+                  img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                  img = np.expand_dims(img, axis = 0)
+                  cv2.circle(frame, (x, y), r, (255, 0, 0), 4)
+                  predict = model.predict(img)
+                  if predict[0][0] > 0.5:
+                     cv2.circle(frame, (x, y), r, (0, 0, 255), 4)
                   else:
-                     delay = i - j
-                  stack.append([delay, y])
-                  print(stack)
-                  j = i
-               else:
-                  cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+                     cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+            snap = False   
 
-   cv2.line(frame, (snap_x, 0), (snap_x, frame.shape[1]), (255, 0, 0), 3)
+      cv2.imshow('frame', frame)
+      cv2.imshow('mask', mask)
+      
+      conn.send(b'none\r\n')
 
-   cv2.imshow('frame', frame)
-   cv2.imshow('mask', mask)
-
-   time.sleep(0.05)
-    
-   conn.send(send_mes)
-
-   root.update()
 
    if cv2.waitKey(1) & 0xFF == ord('q'):
       break
--- robot.py
+++ robot.py
@@ -1,37 +1,25 @@
 import socket
+from tkinter import *
+
+
+translation = b'none'
+
+
+def send_mes():
+    global translation
+    translation = b'snap\r\n'
 
 
 s = socket.socket()
-s.connect(('127.0.0.1', 9090))
+s.connect(('192.168.1.241', 502))
 print('Robot ready')
 
-i = 0
-j = 0
-delay = 5
-y = 0
-y_prev = 0
+root = Tk()
+Button(text='snap', command=send_mes).place(x=10, y=0)
+
 
 while True:
-    if i == j + delay:
-        s.send(b'robot move')
-        if y != y_prev or delay > 0:
-            if delay == 0:
-                delay = 2
-            print('robot move, y = ', y, 'delay = ', delay)
-            y_prev = y
-    else:
-        s.send(str.encode(str(i)))
-        
-    i += 1
-
-    if i == 1000:
-        i = 0
-
-    mes = s.recv(1024)
-
-    if mes != b'none':
-        line = mes.decode('utf-8').split(',')
-        delay = int(line[0])
-        y = int(line[1])
-        j = i
-        
+    if translation != b'none\r\n':
+        s.send(translation)
+        translation = b'none\r\n'
+    root.update()    
